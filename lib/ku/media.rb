20a58493843664947975ce09b49400591bbbe3ce
jamiehodge <jamiehodge@me.com>
2012-09-13 14:40:54 +0200

    Alter require order

--- lib/ku/media.rb
+++ lib/ku/media.rb
@@ -2,6 +2,9 @@ require 'bundler/setup'
 
 require_relative 'media/config/sequel'
 
+require_relative 'media/person'
+require_relative 'media/group'
+
 require_relative 'media/link'
 require_relative 'media/asset'
 
@@ -17,6 +20,3 @@ require_relative 'media/proxy'
 
 require_relative 'media/license'
 require_relative 'media/consent'
-
-require_relative 'media/person'
-require_relative 'media/group'
